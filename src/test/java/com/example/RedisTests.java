2dc920161d8a13fd25857d479df3a4b98c1fc9df
285980382@qq.com <285980382@qq.com>
2017-04-18 22:24:40 +0800

    support redis protocol at see http://www.cnblogs.com/smark/p/3247620.html

--- src/test/java/com/example/RedisTests.java
+++ src/test/java/com/example/RedisTests.java
@@ -1,6 +1,6 @@
 package com.example;
 
-import com.example.sdk.ClientSdk;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +8,20 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+/**
+ * java系统
+ *  日志级别error
+ *  处理1000*7次读写操作（覆盖kv list hash） 耗时3072ms 3100ms
+ *  读4000*3次耗时19s
+ * redis系统
+ *  处理1000*7次读写操作（覆盖kv list hash） 耗时3122ms 2930ms
+ *  读4000*3次耗时19s
+ *
+ *  性能比拼接近于等价，争取超过redis！fighting！！
+ */
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = DemoApplication.class)
+@Slf4j
 public class RedisTests {
 
     @Autowired
@@ -17,13 +29,15 @@ public class RedisTests {
 
     @Test
     public void testR() throws Exception {
-        //template.opsForValue().set("aa", "b");
-        //System.out.println("k=aa,v=" + template.opsForValue().get("aa"));
-        //template.opsForList().leftPush("list", "1");
-        //System.out.println("list=" + template.opsForList().range("list", 0, -1));
-//        template.opsForHash().put("user200", "age", "25");
-//        template.opsForHash().put("user200", "sex", "男");
-        System.out.println("hv="+template.opsForHash().get("user200", "age"));
+        for (int i = 0; i < 4000; ++i) {
+            //template.opsForValue().set("aa" + i, "b" + i);
+            log.debug("k=aa" + i + ",v=" + template.opsForValue().get("aa" + i));
+            //template.opsForList().leftPush("list", "1");
+            log.debug("list=" + template.opsForList().range("list", 0, -1));
+            //template.opsForHash().put("user200" + i, "age", "25");
+            //template.opsForHash().put("user200" + i, "sex", "男");
+            log.debug("hv=" + template.opsForHash().get("user200" + i, "age"));
+        }
     }
 
 }
